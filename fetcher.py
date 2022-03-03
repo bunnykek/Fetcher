4819e3c76e8046ce4c819734e29ba4d5377eafee
bunny <67633271+bunnykek@users.noreply.github.com>
2022-03-03 14:51:22 +0530

    New features and updates.
    1. Now you can select any audio track of your wish rather than the previous, only the most popular one.
    2. Now there are two available dimensions for the artwork | Tall (2048 x 2732) and Square (3840 x 3840).
    
    Previously I was using web scraping methodology and was also working fine but recently due to some HTML changes in the apple music web, the program was throwing an error. So now it is using AppleMusic web api.
    
    You might face auth bearer token error in case if the token given in the script expires, I am trying to find a way to fetch the token automatically.

--- fetcher.py
+++ fetcher.py
@@ -1,44 +1,102 @@
-import requests
-import re
-import os
-import subprocess
-import bs4
-import lxml
-import m3u8
+#by bunny
+
+import requests, re, os, subprocess, m3u8, sys
 from shutil import move
 from mutagen.mp4 import MP4
 
+
+title = """
+             /$$$$$$$$          /$$               /$$                          
+            | $$_____/         | $$              | $$                          
+            | $$     /$$$$$$  /$$$$$$    /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$ 
+            | $$$$$ /$$__  $$|_  $$_/   /$$_____/| $$__  $$ /$$__  $$ /$$__  $$
+            | $$__/| $$$$$$$$  | $$    | $$      | $$  \ $$| $$$$$$$$| $$  \__/
+            | $$   | $$_____/  | $$ /$$| $$      | $$  | $$| $$_____/| $$      
+            | $$   |  $$$$$$$  |  $$$$/|  $$$$$$$| $$  | $$|  $$$$$$$| $$      
+            |__/    \_______/   \___/   \_______/|__/  |__/ \_______/|__/      
+                    Apple-Music animated cover artwork downloader                      
+                                                                -- by bunny  
+    """
+
+def get_auth_token():
+    #response = requests.get("https://k0ybdlmho9.execute-api.ap-northeast-1.amazonaws.com/prod/tokens/applemusic/generate")
+    #return(response.json()['token'])
+    return('eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldlYlBsYXlLaWQifQ.eyJpc3MiOiJBTVBXZWJQbGF5IiwiaWF0IjoxNjQ0OTQ5MzI0LCJleHAiOjE2NjA1MDEzMjR9.3RUn173ddFRpam0ksOFS-vJFR-wCtJHzcSdGr7exxFQScWxzQxHGht4wyt6iJQqNcEOR4BRmv6O4-2B4jzrGsQ')
+
+def get_album_json(country, alb_id, token):
+    
+    headers = {
+        'authorization': f'Bearer {token}'
+    }
+
+    params = (
+        ('filter[equivalents]', f'{alb_id}'),
+        ('extend', 'offers,editorialVideo'),
+    )
+
+    response = requests.get(f'https://amp-api.music.apple.com/v1/catalog/{country}/albums', headers=headers, params=params)
+    return(response.json())
+
+def get_m3u8(json, atype):
+    if atype == 'T' or 't':
+        return(json['data'][0]['attributes']['editorialVideo']['motionDetailTall']['video'])
+    else:
+        return(json['data'][0]['attributes']['editorialVideo']['motionDetailSquare']['video'])
+
+
+def listall(json):
+    totaltracks = int(json['data'][0]['attributes']['trackCount'])
+    for i in range(totaltracks):
+        if json['data'][0]['relationships']['tracks']['data'][i]['type'] == "songs":
+            song = json['data'][0]['relationships']['tracks']['data'][i]['attributes']['name']
+            print(f"        {i+1}. {song}")
+
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+def check_token(tkn):
+    headers = {
+        'authorization': f'Bearer {tkn}'
+    }
+
+    params = (
+        ('filter[equivalents]', '1551901062'),
+        ('extend', 'editorialVideo'),
+    )
+
+    response = requests.get('https://amp-api.music.apple.com/v1/catalog/in/albums', headers=headers, params=params)
+
+    if response.status_code != 200:
+        raise ValueError(401)
+
+
+
 if __name__ == "__main__":
+    
     #clean screen
     if os.name == 'nt':
         subprocess.call("cls", shell=True) # windows
     else:
         subprocess.call("clear") # linux/mac
     
-    ####
-    title = """
-         _    __  __       _          _                 _           _                              
-        / \  |  \/  |     / \   _ __ (_)_ __ ___   __ _| |_ ___  __| |                             
-       / _ \ | |\/| |    / _ \ | '_ \| | '_ ` _ \ / _` | __/ _ \/ _` |                             
-      / ___ \| |  | |   / ___ \| | | | | | | | | | (_| | ||  __/ (_| |                             
-     /_/   \_\_|  |_|  /_/   \_\_| |_|_|_| |_| |_|\__,_|\__\___|\__,_|                             
-                        _         _                      _         __      _       _               
-                       / \   _ __| |___      _____  _ __| | __    / _| ___| |_ ___| |__   ___ _ __ 
-                      / _ \ | '__| __\ \ /\ / / _ \| '__| |/ /   | |_ / _ \ __/ __| '_ \ / _ \ '__|
-                     / ___ \| |  | |_ \ V  V / (_) | |  |   <    |  _|  __/ || (__| | | |  __/ |   
-                    /_/   \_\_|   \__| \_/\_/ \___/|_|  |_|\_\   |_|  \___|\__\___|_| |_|\___|_|   
-                                                                                                   -- by bunny  
-    """
-    
     print(title)
+
+    token = get_auth_token()
+    print("Checking if the token is still alive...")
+    #checking if the token is still alive
+    try:
+        check_token(token)
+    except Exception as e:
+        print("Dead token :/")
+        sys.exit()
     
-    
-    #regex crap
-    regex="https://mvod.itunes.apple.com/itunes-assets/HLSMusic\d{3}/v4/\w{2}/\w{2}/\w{2}/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/\w{10}_default.m3u8"
-    m4a_regex = "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview\w{3}/\w{2}/\w{2}/\w{2}/\w{2}/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/\w+.plus.aac.ep.m4a"
-    
-    #Takes link from user
+    print("good!")
+
+    #Taking input from the user
     url = input("Enter the Album URL : ")
+    artwork_type = input("Do you need the (T)aller or (S)quarer one : ")
     rep = input("Number of times you want to loop the artwork (recommended = 2)  : ")
     aud = input("Do you also need the audio in the artwork? [y/n] : ")
     
@@ -46,53 +104,55 @@ if __name__ == "__main__":
     country = re.search("/\D\D/",url).group().replace("/","")
     id_ = re.search("\d+",url).group()
     
-    #get request to the album url
-    response = requests.get(url)
+    album_json = get_album_json(country, id_, token)
     
-    #making the soup :D
-    soup = bs4.BeautifulSoup(response.text,"lxml")
-    
-    #extracting the master m3u8 from html using regular expression
-    m3u8_ = re.search(regex,response.text).group()
-    
-    #using itunes unofficial API for getting album metadata
-    metadata = requests.get(f"https://itunes.apple.com/lookup?id={id_}&country={country}&entity=album").json()
+
+    #extracting the master m3u8 from album_json
+    m3u8_ = get_m3u8(album_json, artwork_type)
     
     
     #metadata crap
-    meta = metadata["results"][0]
+    meta = album_json['data'][0]['attributes']
     
-    album = meta["collectionName"]
+    album = meta["name"]
     artist = meta["artistName"]
-    album_url = meta["collectionViewUrl"]
-    rating = meta["collectionExplicitness"]
+    album_url = meta["url"]
+    rating = meta["contentRating"]
     tracks = meta["trackCount"]
     copyright_ = meta["copyright"]
     release_date = meta["releaseDate"]
-    genre = meta["primaryGenreName"]
-    
+    genre = meta["genreNames"][0]
+    upc = meta['upc']
+    record_label = meta['recordLabel']
+    editorial_notes = ''
+    try:
+        editorial_notes = meta['editorialNotes']['standard']
+    except:
+        pass
     #showing general details
     metadata = f"""\n
-    Album Name       : {album}
-    Artist           : {artist}
-    Rating           : {rating}
-    Number of tracks : {tracks}
-    Copyright        : {copyright_}
-    Release date     : {release_date}
-    Genre            : {genre}
-    Album URL        : {album_url}
+        Album Name       : {album}
+        Artist           : {artist}
+        Rating           : {rating}
+        Number of tracks : {tracks}
+        Copyright        : {copyright_}
+        Release date     : {release_date}
+        Genre            : {genre}
+        URL              : {album_url}
     """
     print(metadata)
     
     
     #file name
     fname = f"{artist} - {album} ({release_date[:4]}).mp4" 
+    current_path = sys.path[0]
     
-    
-    #getting the popular track m4a link
-    popular = int(soup.find("button", {"aria-label": "Popular Track, Loved"}).parent.parent.parent['data-row'])
-    m4a = re.findall(m4a_regex,response.text)[popular]
-    
+    try:
+        os.makedirs(os.path.join(current_path,"Animated artworks"))
+    except:
+         pass
+        
+    video_path = os.path.join(current_path,"Animated artworks",fname)
     
     #parsing the playlist and getting the max res hevc containing m3u8 file
     playlist = m3u8.load(m3u8_)
@@ -110,39 +170,51 @@ if __name__ == "__main__":
     
     
     if(aud=="Y" or aud =="y"):
-        #downloading the popular m4a track
+
+        print("All the audio tracks:\n")
+        listall(album_json)
+        index = int(input("\nSelect the audio track number : "))
+        index = index - 1
+
+        m4a = album_json['data'][0]['relationships']['tracks']['data'][index]['attributes']['previews'][0]['url']
+
+        #downloading the selected m4a track
         print("\nDownloading the audio...")
         subprocess.Popen(["ffmpeg","-y","-i",m4a,"-acodec","copy","audio.m4a"],stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT).wait()
         print("Audio downloaded.")
     
         #multiplexing
         print("\nMultiplexing...")
-        subprocess.Popen(["ffmpeg","-y","-i","fixed.mp4","-i","audio.m4a","-map_metadata","0" ,"-fflags", "+bitexact" ,"-flags:v" ,"+bitexact","-flags:a", "+bitexact","-c","copy","-shortest",fname],stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT).wait()
+        subprocess.Popen(["ffmpeg","-y","-i","fixed.mp4","-i","audio.m4a","-map_metadata","0" ,"-fflags", "+bitexact" ,"-flags:v" ,"+bitexact","-flags:a", "+bitexact","-c","copy","-shortest",video_path],stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT).wait()
         print("Done.")
     
         os.remove("fixed.mp4")
         os.remove("audio.m4a")
     
     else:
-        move("fixed.mp4", fname)
+        move("fixed.mp4", video_path)
     
     
     #tagging
     
     print("\nTagging metadata..")
-    video = MP4(fname)
+    video = MP4(video_path)
     video["\xa9alb"] = album
-    video["\xa9ART"] = artist
+    video["aART"] = artist
     video["----:TXXX:URL"] = bytes(album_url,'UTF-8')
-    video["----:TXXX:Rating"] = bytes(rating,'UTF-8')
+    video["----:TXXX:Content Advisory"] = bytes(rating,'UTF-8')
     video["----:TXXX:Total tracks"] = bytes(str(tracks),'UTF-8')
     video["cprt"] = copyright_
     video["----:TXXX:Release date"] = bytes(release_date,'UTF-8')
+    video["----:TXXX:UPC"] = bytes(upc,'UTF-8')
+    video["----:TXXX:Record label"] = bytes(record_label,'UTF-8')
     video["\xa9gen"] = genre
+    if editorial_notes != '':
+        video["----:TXXX:Editorial notes"] = bytes(remove_html_tags(editorial_notes),'UTF-8')
     video.save()
     print("Done.")
     
-    print (os.path.join(os.getcwd(),fname))
+    print ('\n'+video_path)
     
     #deleting temp files
     os.remove("video.mp4")
